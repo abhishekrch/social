77196197451889aa3f8c1568324401713a43ab05
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-01-23 00:12:32 +0530

    feat(models): add Posts schema

---
+++ server/models/Posts.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 280
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  comments: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    content: String,
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }]
+}, {
+  timestamps: true
+});
+
+const Post = mongoose.model('Post', postSchema);
+export default Post; 
